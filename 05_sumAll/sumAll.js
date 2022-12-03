367419a22dcf03f05a09e50b2df986d73cb24a66
Kyle Weaver <kyle.weaver@sita.aero>
2022-12-03 20:00:52 +0100

    Added error for non-number params

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,6 @@
 const sumAll = function(x, y) {
     if (x < 0 || y < 0) return 'ERROR'
+    if (typeof x != 'number' || typeof y != 'number') return 'ERROR'
     if (x > y){
         let z = x;
         x = y;
