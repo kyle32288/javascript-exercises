7865256ff8742e7aa6a8fd962e7cf549fa5c8477
Kyle Weaver <kyle.weaver@sita.aero>
2022-12-03 12:11:22 +0100

    updated removeFromArray to only filter elements of the same type

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,6 @@
 const removeFromArray = function(myArray, removeMe) {
     for (let i = 1; i < arguments.length; i++){
-        myArray = myArray.filter(x => x != arguments[i]);
+        myArray = myArray.filter(x => x != arguments[i] || typeof(arguments[i]) != typeof(x));
     }
     return myArray;
 };
