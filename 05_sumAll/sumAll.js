d085fe4929377f62d7c65b86fb2838e4a9c3ec4e
Kyle Weaver <kyle.weaver@sita.aero>
2022-12-03 12:22:18 +0100

    Updated sumAll to work when X is bigger than Y

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,10 @@
 const sumAll = function(x, y) {
     let sum = 0;
+    if (x > y){
+        let z = x;
+        x = y;
+        y = z;
+    }
     for (let i = x; i <= y; i++){
         sum += i;
     }
