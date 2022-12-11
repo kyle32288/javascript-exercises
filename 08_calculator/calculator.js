578769cff6195bebf606c0571b94b51a244b56ba
kyle32288 <kyle@weavs.us>
2022-12-11 12:51:18 +0100

    calculator passes all tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
 const add = function() {
-	
+  const myArray = Array.from(arguments)
+  return myArray.reduce((x, y) => x + y, 0);
 };
 
 const subtract = function() {
-	
+  const myArray = Array.from(arguments)
+  return myArray.reduce((x, y) => x - y);
 };
 
-const sum = function() {
-	
+const sum = function(myArray) {
+	return myArray.reduce((x, y) => x + y, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(myArray) {
+  return myArray.reduce((x, y) => x * y);
 };
 
-const power = function() {
-	
+const power = function(x,y) {
+	return (x ** y)
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  const myArray = [];
+  for (i = 1; i <= x; i++){
+    myArray.push(i)
+  }
+  return myArray.reduce((total, num) => total * num, 1);
 };
 
 // Do not edit below this line
