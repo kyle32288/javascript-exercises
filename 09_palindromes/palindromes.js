76b2ab52cd7437a03dc76a955065e242842320e7
kyle32288 <kyle@weavs.us>
2022-12-12 14:51:41 +0100

    palindromes passes all tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,7 @@
-const palindromes = function () {
-
-};
-
+const palindromes = function (string) {
+    myArray = Array.from(string.toLowerCase());
+    myArray = myArray.filter(c => ((/[a-zA-Z]/).test(c)));
+    return myArray.join('') == myArray.reverse().join('');
+}
 // Do not edit below this line
 module.exports = palindromes;
