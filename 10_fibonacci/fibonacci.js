b070dd99b986ed62ac0ad42769c18439582aef46
kyle32288 <kyle@weavs.us>
2022-12-14 13:26:58 +0100

    fibonacci passes all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num == 1) return(1)
+    if (num <= 0) return('OOPS')
+    let x = 0;
+    let y = 1;
+    let z;
+    for (let i = 0; i < +num-1; i++){
+        z = x + y;
 
+        x = y;
+        y = z;  
+    }
+    return(z)
 };
 
 // Do not edit below this line
