b69d0306d98c871ea6ed59bffcaca6d94f880146
kyle32288 <kyle@weavs.us>
2022-12-14 13:50:11 +0100

    findTheOldest passes all tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,7 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    people.forEach(p => {if (!p.yearOfDeath) p.yearOfDeath = new Date().getFullYear();})
+    people.sort((a, b) => (a.yearOfDeath - a.yearOfBirth) < (b.yearOfDeath - b.yearOfBirth) ? 1 : -1);
+    return people[0]
 
 };
 
